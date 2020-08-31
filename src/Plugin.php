353fa809e54e230ea3d676a23c64f79f6d6dd85a
Andreas Heigl <andreas@heigl.org>
2020-08-31 08:01:34 +0200

    Add post-install hook
    Now the plugin can download phar files from a specific repository.
    
    Ich you now install `org_heigl/hyphenator` it will try to download an
    artifact in the specified version.

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,16 +12,29 @@ namespace Org_Heigl\TestComposerPlugin;
 
 use Closure;
 use Composer\Composer;
+use Composer\DependencyResolver\GenericRule;
 use Composer\DependencyResolver\Request;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\Installer\InstallerEvent;
 use Composer\Installer\InstallerEvents;
+use Composer\Installer\PackageEvent;
+use Composer\Installer\PackageEvents;
 use Composer\IO\IOInterface;
-use Composer\Plugin\Capable;
+use Composer\Package\CompletePackage;
 use Composer\Plugin\PluginInterface;
+use Composer\Semver\Constraint\ConstraintInterface;
+use Composer\Semver\Constraint\MultiConstraint;
+use function get_class;
+use function var_dump;
 
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
+    private const PLUGIN_NAME = 'org_heigl/hyphenator';
+
+    private const BINARY_NAME = 'testbinary';
+
+    private const PHAR_FILE_URI = 'https://github.com/heiglandreas/Org_Heigl_Hyphenator/archive/%version%.zip';
+
     private $composer;
 
     private $io;
@@ -37,6 +50,12 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         return [
             InstallerEvents::PRE_DEPENDENCIES_SOLVING => [
                 ['preDependenciesSolving', 0],
+            ],
+            PackageEvents::POST_PACKAGE_INSTALL => [
+                ['postPackageEvent', 0],
+            ],
+            PackageEvents::POST_PACKAGE_UPDATE => [
+                ['postPackageEvent', 0],
             ],
         ];
     }
@@ -58,8 +77,32 @@ class Plugin implements PluginInterface, EventSubscriberInterface
                 unset($jobs[$key]);
             }
         }
-        $this->io->write(print_r($event->getRequest()->getJobs(), true));
+        //$this->io->write(print_r($event->getRequest()->getJobs(), true));
     }
 
+    public function postPackageEvent(PackageEvent $event): void
+    {
+        /** @var GenericRule $rule */
+        $rule = $event->getOperation()->getReason();
+        /** @var MultiConstraint $constraint */
+        $constraint = $rule->getJob()['constraint'];
+        if ($rule->getRequiredPackage() !== self::PLUGIN_NAME) {
+            return;
+        }
 
+        /** @var CompletePackage $packages */
+        $package = $event->getInstalledRepo()->findPackage($rule->getRequiredPackage(), $constraint->getPrettyString());
+        $package->getName();
+
+        $this->io->write(sprintf(
+            'downloading Artifact in version %2$s from %1$s',
+            str_replace('%version%', $package->getFullPrettyVersion(), self::PHAR_FILE_URI),
+            $package->getFullPrettyVersion()
+        ));
+
+        $this->io->write('downloading signature for PHAR-File');
+        $this->io->write('verifying PHAR-File with contained public keys');
+        $this->io->write('installing PHAR-File');
+        $this->io->write(sprintf('You can now use `./vendor/bin/%1$s` to run the package', self::BINARY_NAME));
+    }
 }
